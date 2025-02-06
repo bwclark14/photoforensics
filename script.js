f83f6a72fc500f7bfa388e3d6943c51d270e52f8
bwclark14 <brianwilsonclark@gmail.com>
2025-02-06 14:35:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageFiles = [
-        "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg",
+        "alice-triquet-HeEJU3nrg_0-unsplash.jpg", "quino-al-mBQIfKlvowM-unsplash", "image3.jpg", "image4.jpg",
         "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg",
         "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg"
     ];
