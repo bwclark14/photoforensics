fa76de7979d135ab55c69be5cf9fb5162d04939d
bwclark14 <brianwilsonclark@gmail.com>
2025-02-06 14:49:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imageFiles = [
-        "cat.jpg", "image2.jpg", "image3.jpg", "image4.jpg",
+        "cat.jpg", "legostreet.jpg", "image3.jpg", "image4.jpg",
         "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg",
         "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg"
     ];
